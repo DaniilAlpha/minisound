2117adf2f9fc89f5e02332c7cbed3f12a7ef456d
Daniil Miskevich <daniilmiskevich@gmail.com>
2024-08-13 12:15:56 +0300

    Closes #11; Delete tmp cmake files.

--- minisound_web/lib/js/CMakeFiles/minisound_web_js.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/Daniil/_work/_projects/_flutter/minisound/minisound_web/lib/build/minisound_web.loader.js"
-  "CMakeFiles/minisound_web_js"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/minisound_web_js.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
